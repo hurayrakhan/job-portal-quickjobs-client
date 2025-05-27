31aa021f181831f31cb05ce7c1ffe765a6f7ea6a
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-05-27 23:48:23 +0600

    setup project router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layout/Root";
+import HomePage from "../Pages/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children:[
+            {
+                path:'/',
+                element: <HomePage></HomePage>
+            }
+        ]
+    }
+]);
+
+export default router;
